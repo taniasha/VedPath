0fe00c7b4e5903cfd12501aca548dc910efef6eb
tania sharma <bishupandit07@gmail.com>
2026-09-05 12:57:00 +0530

    style: format AdminPanel.jsx

--- frontend/src/admin/AdminPanel.jsx
+++ frontend/src/admin/AdminPanel.jsx
@@ -11,90 +11,90 @@ import { useAuth } from '../context/AuthContext';
 export default function AdminPanel() {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState('books');
-  const {logout} = useAuth();
+  const { logout } = useAuth();
 
-  const handleLogout=()=>{
-     logout();
-     toast.success("Logout successful");
-     navigate('/login');
+  const handleLogout = () => {
+    logout();
+    toast.success("Logout successful");
+    navigate('/login');
   }
 
   const renderContent = () => {
     switch (activeTab) {
       case 'books':
-        return <AdminBooks/>;
+        return <AdminBooks />;
       case 'trending':
         return <AdminTrending />;
       case 'audio':
         return <AdminAudio />;
       case 'orders':
-        return <AdminOrders/>;
-     case 'users':
-        return <AdminUsers/>;
+        return <AdminOrders />;
+      case 'users':
+        return <AdminUsers />;
       default:
         return <p>Select an option from the left menu.</p>;
     }
   };
 
- return (
-  <div className="container-fluid"> 
-    <div className="row" style={{ height: '100vh', overflow: 'hidden' }}>
+  return (
+    <div className="container-fluid">
+      <div className="row" style={{ height: '100vh', overflow: 'hidden' }}>
 
-      {/* Sidebar Navbar */}
-      <nav
-        className="col-md-3 col-lg-2 bg-light sidebar shadow-sm py-4"
-        style={{ height: '100%', overflowY: 'auto' }}
+        {/* Sidebar Navbar */}
+        <nav
+          className="col-md-3 col-lg-2 bg-light sidebar shadow-sm py-4"
+          style={{ height: '100%', overflowY: 'auto' }}
         >
-        <h3 className="text-center mb-4 fw-bolder" style={{color:'#863434ff'}}>Admin Menu</h3>
-        <hr />
-
-        <ul className="nav flex-column ">
-          <li className="nav-item">
-            <button onClick={() => setActiveTab('books')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'books' ? 'active text-primary' : ''}`}>
-            <i class="bi bi-book-fill admin-icon fs-3"></i> Insert Books
-            </button>
-          </li>
+          <h3 className="text-center mb-4 fw-bolder" style={{ color: '#863434ff' }}>Admin Menu</h3>
           <hr />
-          <li className="nav-item">
-            <button onClick={() => setActiveTab('trending')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'trending' ? 'active text-primary' : ''}`}>
-             <i class="bi bi-fire admin-icon fs-3"></i> Insert Trending
-            </button>
-          </li>
-           <hr />
-          <li className="nav-item">
-            <button onClick={() => setActiveTab('audio')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'audio' ? 'active text-primary' : ''}`}>
-              <i className="bi bi-headphones fs-3 admin-icon"></i> Insert Audio
-            </button>
-          </li> 
-           <hr />
-          <li className="nav-item">
-            <button onClick={() => setActiveTab('orders')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'orders' ? 'active text-primary' : ''}`}>
-              <i class="bi bi-box-fill admin-icon fs-3"></i> Order Tracking
-            </button>
-          </li>
-          <hr />
-          <li className="nav-item">
-            <button onClick={() => setActiveTab('users')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'users' ? 'active text-primary' : ''}`}>
-              <i class="bi bi-people-fill admin-icon fs-3"></i> Signup Users
-            </button>
-          </li>
-        </ul>
-      </nav>
 
-      {/* Dynamic Component Area (Scrollable) */}
-      <main className="col-md-9 ms-sm-auto col-lg-10 py-4"
-        style={{ height: '100%', overflowY: 'auto' }}>
-           <div className="d-flex justify-content-end px-3">
-                <button className="btn btn-outline-danger mb-3" onClick={handleLogout}>Logout</button>
-           </div>
+          <ul className="nav flex-column ">
+            <li className="nav-item">
+              <button onClick={() => setActiveTab('books')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'books' ? 'active text-primary' : ''}`}>
+                <i class="bi bi-book-fill admin-icon fs-3"></i> Insert Books
+              </button>
+            </li>
+            <hr />
+            <li className="nav-item">
+              <button onClick={() => setActiveTab('trending')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'trending' ? 'active text-primary' : ''}`}>
+                <i class="bi bi-fire admin-icon fs-3"></i> Insert Trending
+              </button>
+            </li>
+            <hr />
+            <li className="nav-item">
+              <button onClick={() => setActiveTab('audio')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'audio' ? 'active text-primary' : ''}`}>
+                <i className="bi bi-headphones fs-3 admin-icon"></i> Insert Audio
+              </button>
+            </li>
+            <hr />
+            <li className="nav-item">
+              <button onClick={() => setActiveTab('orders')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'orders' ? 'active text-primary' : ''}`}>
+                <i class="bi bi-box-fill admin-icon fs-3"></i> Order Tracking
+              </button>
+            </li>
+            <hr />
+            <li className="nav-item">
+              <button onClick={() => setActiveTab('users')} className={`nav-link btn btn-link w-100 text-start ${activeTab === 'users' ? 'active text-primary' : ''}`}>
+                <i class="bi bi-people-fill admin-icon fs-3"></i> Signup Users
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Dynamic Component Area (Scrollable) */}
+        <main className="col-md-9 ms-sm-auto col-lg-10 py-4"
+          style={{ height: '100%', overflowY: 'auto' }}>
+          <div className="d-flex justify-content-end px-3">
+            <button className="btn btn-outline-danger mb-3" onClick={handleLogout}>Logout</button>
+          </div>
 
           <div className="px-3">
             {renderContent()}
           </div>
-      </main>
+        </main>
 
+      </div>
     </div>
-  </div>
-);
+  );
 
 }
